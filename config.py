7ee6f25e3ec852d352815ed39ee14aac165dfd84
Tully22 <harry_tull@hotmail.com>
2021-11-19 11:00:57 +0000

    Added script files

---
+++ config.py
@@ -0,0 +1,9 @@
+#bot info from reddit
+client_id = 'xxx'
+client_secret = 'xxx'
+username = 'danielplainviewbot' 
+password = 'xxx'
+user_agent = 'replybot by /u/danielplainviewbot'
+subreddit_name = 'all' #goes through r/all change if you want to a specific subreddit
+trigger = 'there will be blood' #edit as needed
+path_to_quotes = './quotes.txt' #edit quotes.txt as needed
